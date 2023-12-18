9c07c99cb45f589506cae116e781fb43fde54b31
Tucnuc <cer42372@spseol.cz>
2023-12-18 22:21:50 +0100

    Release 1.1

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
     var manhwaSelector = document.getElementById("manhwaSelector");
     var images = [];
 
-    for (var i = 1; i <= 22; i++) {
+    for (var i = 1; i <= 80; i++) {
         images.push(document.getElementById("image" + i));
     }
 
@@ -10,8 +10,8 @@ document.addEventListener("DOMContentLoaded", function() {
         var selectedValue = manhwaSelector.value;
 
         if (selectedValue === "barolt") {
-            for (var i = 1; i <= 22; i++) {
-                images[i - 1].src = "barolt_vs_garhan/" + i + ".webp";
+            for (var i = 1; i <= 80; i++) {
+                images[i - 1].src = "barolt_vs_garhan/" + i + ".jpg";
             }
         } else {
         }

97c635ce5201aab295357a5db0645737e0ad2c1c
Tucnuc <cer42372@spseol.cz>
2023-12-19 21:52:18 +0100

    Release 2.0

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Manhwa Fights
 Na této stránce lze vybrat pomocí HTML select tagu jeden z několika soubojí z mých oblíbených komiksů. <br/>
 Následně se načte celý souboj bez jakéhokoli přerušování, jak by tomu bylo při normálním čtení. <br/><br/>
-Např.: Různé flashbacky, konce kapitol, atd
+Např.: Různé až moc dlouhé flashbacky, konce chapterů, atd
--- app.js
+++ app.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
     var manhwaSelector = document.getElementById("manhwaSelector");
     var images = [];
 
-    for (var i = 1; i <= 80; i++) {
+    for (var i = 1; i <= 112; i++) {
         images.push(document.getElementById("image" + i));
     }
 
@@ -10,7 +10,7 @@ document.addEventListener("DOMContentLoaded", function() {
         var selectedValue = manhwaSelector.value;
 
         if (selectedValue === "barolt") {
-            for (var i = 1; i <= 80; i++) {
+            for (var i = 1; i <= 112; i++) {
                 images[i - 1].src = "barolt_vs_garhan/" + i + ".jpg";
             }
         } else {
